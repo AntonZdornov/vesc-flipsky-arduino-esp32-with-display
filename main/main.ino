0fb5490ee2cf762cd06288bc1926fc63a5178755
anton <anton@Antons-MacBook-Pro.local>
2025-10-20 18:20:19 +0300

    init project

--- main/main.ino
+++ main/main.ino
@@ -3,29 +3,13 @@
 #include <VescUart.h>
 #include "display.h"
 #include "lvgl_driver.h"
-// #include "wifi_service.h"
-// #include "ble_service.h"
 #include "logger.h"
 #include "utils.h"
-// #include <WiFi.h>
 #include "ui_globals.h"
-// #include <WiFiClientSecure.h>
-// #include <HTTPClient.h>
-// #include <ArduinoJson.h>
 #include <lvgl.h>
 
-// WIFISettings
-// WiFiClient client;
-
-const char *YT_API_KEY = "";
-const char *YT_CHANNEL_ID = "";
-const unsigned long REFRESH_MS = 60000;
 unsigned long lastFetch = 0;
 
-// UI
-#define SRC_W 250
-#define SRC_H 250
-static lv_color_t src_buf[SRC_W * SRC_H];
 lv_obj_t *root_container;
 lv_obj_t *label_views = nullptr;
 lv_obj_t *label_subscribers = nullptr;
@@ -33,69 +17,6 @@ extern const lv_font_t lv_font_montserrat_48;
 extern const lv_font_t lv_font_montserrat_38;
 extern const lv_font_t lv_font_montserrat_12;
 extern const lv_font_t lv_font_montserrat_22;
-
-// bool fetchStats(uint64_t &subs, uint64_t &views) {
-//   WiFiClientSecure client;
-//   client.setTimeout(15000);
-//   // Для простоты: отключаем проверку сертификата. В проде лучше задать корневой сертификат.
-//   client.setInsecure();
-
-//   HTTPClient https;
-//   String url = String("https://www.googleapis.com/youtube/v3/channels")
-//                + "?part=statistics&id=" + YT_CHANNEL_ID + "&key=" + YT_API_KEY;
-
-//   if (!https.begin(client, url)) {
-//     LOG_PRINTLN("HTTPS begin() failed");
-//     return false;
-//   }
-
-//   int code = https.GET();
-//   if (code != HTTP_CODE_OK) {
-//     LOG_PRINTF("HTTP error: %d\n", code);
-//     https.end();
-//     return false;
-//   }
-
-//   // 1) читаем весь ответ в строку
-//   String payload = https.getString();
-//   https.end();
-
-//   // 2) выводим для отладки
-//   // LOG_PRINTLN(payload);
-
-//   // 3) на всякий случай подчистим и парсим из строки (НЕ из потока!)
-//   payload.trim();
-//   int brace = payload.indexOf('{');  // если вдруг есть мусор до JSON
-//   if (brace > 0) payload = payload.substring(brace);
-
-//   DynamicJsonDocument doc(4096);  // буфер побольше
-//   DeserializationError err = deserializeJson(doc, payload);
-
-//   // // Парсим JSON
-//   // DynamicJsonDocument doc(8192);  // хватает для statistics
-//   // DeserializationError err = deserializeJson(doc, https.getStream());
-//   https.end();
-//   if (err) {
-//     LOG_PRINTF("JSON error: %s\n", err.c_str());
-//     return false;
-//   }
-
-//   // Путь: items[0].statistics.subscriberCount / viewCount
-//   if (!doc["items"] || doc["items"].size() == 0) {
-//     LOG_PRINTLN("No items in response");
-//     return false;
-//   }
-
-//   JsonObject stats = doc["items"][0]["statistics"];
-//   const char *subStr = stats["subscriberCount"] | "0";
-//   const char *viewStr = stats["viewCount"] | "0";
-
-//   // 64-бит, потому что просмотры бывают очень большие
-//   subs = strtoull(subStr, nullptr, 10);
-//   views = strtoull(viewStr, nullptr, 10);
-
-//   return true;
-// }
 
 #define VESC_RX_PIN 4  // VESC RX
 #define VESC_TX_PIN 5  // VESC TX
@@ -123,8 +44,6 @@ void createUI() {
                         LV_FLEX_ALIGN_CENTER,         // горизонтально по центру
                         LV_FLEX_ALIGN_SPACE_BETWEEN,  // вертикально вниз
                         LV_FLEX_ALIGN_CENTER);
-
-  make_logo_img(root_container, 0, 0.5f, /*dst center*/ 60, 60);
 
   lv_obj_t *views_group = lv_obj_create(root_container);
   lv_obj_set_height(views_group, LV_PCT(20));  // или LV_SIZE_CONTENT + grow
@@ -177,20 +96,20 @@ void createUI() {
   // lv_obj_set_style_pad_all(subscribers_group, 0, LV_PART_MAIN);
   // lv_obj_set_style_border_width(subscribers_group, 0, LV_PART_MAIN);
 
-  lv_obj_t *label_subscribers_title = lv_label_create(views_group);
-  lv_label_set_text(label_subscribers_title, "Subscribers");
-  lv_obj_set_width(label_subscribers_title, LV_PCT(100));
-  lv_obj_set_style_text_align(label_subscribers_title, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_set_style_text_color(label_subscribers_title, lv_color_white(), 0);
-  lv_obj_set_style_text_font(label_subscribers_title, &lv_font_montserrat_22, 0);
-  lv_obj_set_style_pad_top(label_subscribers_title, 5, LV_PART_MAIN);  // Отступ сверху
+  // lv_obj_t *label_subscribers_title = lv_label_create(views_group);
+  // lv_label_set_text(label_subscribers_title, "Subscribers");
+  // lv_obj_set_width(label_subscribers_title, LV_PCT(100));
+  // lv_obj_set_style_text_align(label_subscribers_title, LV_TEXT_ALIGN_CENTER, 0);
+  // lv_obj_set_style_text_color(label_subscribers_title, lv_color_white(), 0);
+  // lv_obj_set_style_text_font(label_subscribers_title, &lv_font_montserrat_22, 0);
+  // lv_obj_set_style_pad_top(label_subscribers_title, 5, LV_PART_MAIN);  // Отступ сверху
 
-  label_subscribers = lv_label_create(views_group);
-  lv_label_set_text(label_subscribers, "0");
-  lv_obj_set_width(label_subscribers, LV_PCT(100));
-  lv_obj_set_style_text_align(label_subscribers, LV_TEXT_ALIGN_CENTER, 0);
-  lv_obj_set_style_text_color(label_subscribers, lv_color_white(), 0);
-  lv_obj_set_style_text_font(label_subscribers, &lv_font_montserrat_38, 0);
+  // label_subscribers = lv_label_create(views_group);
+  // lv_label_set_text(label_subscribers, "0");
+  // lv_obj_set_width(label_subscribers, LV_PCT(100));
+  // lv_obj_set_style_text_align(label_subscribers, LV_TEXT_ALIGN_CENTER, 0);
+  // lv_obj_set_style_text_color(label_subscribers, lv_color_white(), 0);
+  // lv_obj_set_style_text_font(label_subscribers, &lv_font_montserrat_38, 0);
 }
 
 // void draw_youtube_logo(lv_obj_t *canvas, int cx, int cy, int w) {
@@ -221,43 +140,6 @@ void createUI() {
 //   poly_dsc.border_width = 0;
 //   lv_canvas_draw_polygon(canvas, pts, 3, &poly_dsc);
 // }
-
-void make_logo_img(lv_obj_t *parent, int angle_deg, float zoom_factor, lv_coord_t x, lv_coord_t y) {
-  // 1) Канва-источник
-  lv_obj_t *src = lv_canvas_create(parent);
-  lv_canvas_set_buffer(src, src_buf, SRC_W, SRC_H, LV_IMG_CF_TRUE_COLOR);
-  lv_canvas_fill_bg(src, lv_color_black(), LV_OPA_TRANSP);  // прозрачный фон
-  lv_obj_add_flag(src, LV_OBJ_FLAG_HIDDEN);                 // не показываем сам source
-
-  // Рисуем логотип по центру источника (ширина 160)
-  // draw_youtube_logo(src, SRC_W / 2, SRC_H / 2, 60);
-
-  // 2) Делаем img-дескриптор, который указывает на память канвы
-  static lv_img_dsc_t src_dsc;  // static, чтобы не исчез
-  src_dsc.header.always_zero = 0;
-  src_dsc.header.w = SRC_W;
-  src_dsc.header.h = SRC_H;
-  src_dsc.data_size = SRC_W * SRC_H * sizeof(lv_color_t);
-  src_dsc.header.cf = LV_IMG_CF_TRUE_COLOR;
-  src_dsc.data = (const uint8_t *)src_buf;
-
-  // 3) Создаём lv_img, задаём источник, угол и зум
-  lv_obj_t *img = lv_img_create(parent);
-  lv_img_set_src(img, &src_dsc);
-
-  // угол в десятых долях градуса: 90° => 900, 180° => 1800
-  lv_img_set_angle(img, angle_deg * 10);
-
-  // масштаб: 256 = 1.0x, 128 = 0.5x, 512 = 2.0x
-  uint16_t zoom = (uint16_t)(zoom_factor * 512.0f);
-  lv_img_set_zoom(img, zoom);
-
-  // Поворот/масштаб вокруг центра
-  lv_img_set_pivot(img, SRC_W / 2, SRC_H / 2);
-
-  // Позиция на экране
-  lv_obj_set_pos(img, x - SRC_W / 2, y - SRC_H / 2);
-}
 
 void setup() {
   LOG_BEGIN(BAUD);
